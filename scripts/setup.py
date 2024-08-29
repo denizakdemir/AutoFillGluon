e20257763ff64ae9cead46ab4db44bfdbc97ed56
Deniz Akdemir <dakdemir@nmdp.org>
2024-08-29 12:54:36 -0400

    debugging installation

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,7 +1,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='scorer',
-    version='0.1',
-    packages=find_packages(),
+    name='autogluonImputer',
+    version='0.1.0',
+    author='Deniz Akdemir',
+    author_email='your.email@example.com',  # Replace with your actual email
+    description='A package for imputing missing data using AutoGluon, with scoring utilities.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dakdemir-nmdp/AutoGluonImputer',
+    packages=find_packages(where='scripts'),  # Specify the directory containing your modules
+    package_dir={'': 'scripts'},  # Map the root package to the 'scripts' directory
+    install_requires=[
+        'autogluon',
+        'autogluon.eda',
+        'autogluon.multimodal',
+        'autogluon.tabular',
+        'autogluon.timeseries',
+        'lifelines',
+        'shap',
+        'pyreadstat',
+        'python-pptx',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
 )
